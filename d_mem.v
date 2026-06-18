23fbcc85b5aee528e2c68b3eda450a89eea4a1ec
Sofia <sofia.sdm@hotmail.com>
2026-06-17 22:59:22 -0300

    Update d_mem.v
    Removed RAM initialization block and redundant integer declaration.

--- d_mem.v
+++ d_mem.v
@@ -15,7 +15,7 @@
 module d_mem #(
     parameter RAM_SIZE = 256 // Tamanho parametrizável da memória de dados
 )(
-	 input wire clock,            // Entrada de Clock global
+	input wire clock,            // Entrada de Clock global
     input wire [31:0] Address,   // Endereço de acesso computado pela ULA (32 bits)
     input wire [31:0] WriteData, // Dado fornecido pelo regfile para ser escrito (32 bits)
     input wire MemWrite,         // Sinal de controle que habilita a escrita na RAM (1 bit)
@@ -25,6 +25,7 @@ module d_mem #(
 
     // Declaração do bloco de memória RAM
     reg [31:0] ram [0:RAM_SIZE-1];
+	integer i;
 
     // Escrita Assíncrona / Combinacional na RAM
     // Em um sistema puramente monociclo assíncrono para RAM, a escrita ocorre direto pelo nível lógico
@@ -37,13 +38,5 @@ module d_mem #(
 	 // Leitura Assíncrona da RAM controlada pelo sinal de leitura (MemRead)
     // Se MemRead for 1, lê o dado da posição; caso contrário, zera a saída
     assign ReadData = (MemRead == 1'b1) ? ram[Address[31:2]] : 32'h00000000;
-	 
-	 // Inicialização da RAM com zeros para evitar valores 'x' (indefinidos) na simulação
-	 integer i;                                     // Variável de controle do laço
-    initial begin                                  // Bloco executado no início da simulação
-        for (i = 0; i < RAM_SIZE; i = i + 1) begin // Percorre todas as posições da RAM
-            ram[i] = 32'h00000000;                 // Zera a posição para evitar valor lixo ('x')
-        end
-    end
 
 endmodule
